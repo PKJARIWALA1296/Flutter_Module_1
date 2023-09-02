d3db6f09a908c99fdbb58ce457565f16414628ac
JARIWALA PRATIK KISHORBHAI <130529667+PKJARIWALA1296@users.noreply.github.com>
2023-09-02 17:53:12 +0530

    Add files via upload

---
+++ assignment_fi_one_info_print.dart
@@ -0,0 +1,8 @@
+void main(){
+  print('* * * * * * * * * * * * * * * * * * * * * * * * * * *');
+  print('*            My Name is Pratik Jariwala             *');
+  print('*        My Birthdate is 12th January 1996          *');
+  print('*               I am 27 years Old.                  *');
+  print('* My address is Palanpur Patiya, Rander road, Surat *');
+  print('* * * * * * * * * * * * * * * * * * * * * * * * * * *');
+}
